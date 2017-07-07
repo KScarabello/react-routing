a6990281e34024c4242ad42bc3aabe718165c2b8
Kim Scarabello <kimberly.scarabello@gmail.com>
2017-07-07 14:46:50 -0600

    mini-project complete

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+
 
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
@@ -16,4 +16,4 @@ ReactDOM.render(
   </BrowserRouter>, 
   document.getElementById('root')
 );
-registerServiceWorker();
+
